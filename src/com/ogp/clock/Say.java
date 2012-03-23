61d1a34a3668eea95f653175dab0c702f87f8f79
Olle Palmgren <olle.palmgren@sonyericsson.com>
2012-03-23 15:33:35 +0100

    Removed the queue, was possible to solve the init problem simpler. Changed onStart to onStartCommand.

--- src/com/ogp/clock/Say.java
+++ src/com/ogp/clock/Say.java
@@ -6,21 +6,24 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.IBinder;
 import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Locale;
 
-public class Say extends Service implements OnInitListener {
+public class Say extends Service implements TextToSpeech.OnInitListener {
 
     public static final String SAY_TEXT = "say.text";
 
-    private static final String TAG = "SayTask";
-
-    private ThreadQueue queue = new ThreadQueue();
+    private static final String TAG = "SayService";
 
     private TextToSpeech tts;
+
+    private boolean initialized = false;
+
+    private String text;
+
+    private HashMap<String, String> speakSettings;
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -32,6 +35,9 @@ public class Say extends Service implements OnInitListener {
         super.onCreate();
         Log.d(TAG, "OnCreate");
 
+        speakSettings = new HashMap<String, String>();
+        speakSettings.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_MUSIC));
+
         tts = new TextToSpeech(this, this);
     }
 
@@ -40,13 +46,7 @@ public class Say extends Service implements OnInitListener {
         super.onDestroy();
         Log.d(TAG, "OnDestroy");
 
-        queue.add(new Runnable() {
-
-            @Override
-            public void run() {
-                tts.shutdown();
-            }
-        });
+        tts.shutdown();
     }
 
     @Override
@@ -54,44 +54,38 @@ public class Say extends Service implements OnInitListener {
         Log.d(TAG, "OnInit");
         if (status != TextToSpeech.SUCCESS) {
             Log.e(TAG, "Could not initialize TextToSpeech.");
-            queue.clear();
             return;
         }
 
         int result = tts.setLanguage(Locale.US);
         if (result == TextToSpeech.LANG_MISSING_DATA) {
             Log.e(TAG, "Language is missing data");
-            queue.clear();
             return;
         } else if (result == TextToSpeech.LANG_NOT_SUPPORTED) {
             Log.e(TAG, "Language is not supported");
-            queue.clear();
             return;
         }
 
-        queue.start();
+        result = tts.speak(text, TextToSpeech.QUEUE_FLUSH, speakSettings);
+        if (result == TextToSpeech.ERROR) {
+            Log.e(TAG, "Failed to speak");
+        }
+        initialized = true;
     }
 
     @Override
-    public void onStart(Intent intent, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStart(intent, startId);
-        Log.d(TAG, "OnStart");
+        Log.d(TAG, "OnStartCommand " + startId);
 
-        final String text = intent.getStringExtra(SAY_TEXT);
-
-        queue.add(new Runnable() {
-
-            @Override
-            public void run() {
-                HashMap<String, String> settings = new HashMap<String, String>();
-                settings.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
-                        String.valueOf(AudioManager.STREAM_MUSIC));
-
-                int result = tts.speak(text, TextToSpeech.QUEUE_ADD, settings);
-                if (result == TextToSpeech.ERROR) {
-                    Log.e(TAG, "Failed to speak");
-                }
+        text = intent.getStringExtra(SAY_TEXT);
+        if (initialized) {
+            int result = tts.speak(text, TextToSpeech.QUEUE_FLUSH, speakSettings);
+            if (result == TextToSpeech.ERROR) {
+                Log.e(TAG, "Failed to speak");
             }
-        });
+        }
+
+        return START_NOT_STICKY;
     }
 }
